821d9df5d2d03b95139dff7d87084be142a894ea
TUuno <31230143@students.liu.edu.lb>
2019-02-14 00:15:16 +0200

    updating i3 config/blocks and adding template/appending template command

---
+++ i3/.i3/firefoxInit.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+i3-msg 'append_layout ~/.i3/firefox.json; exec firefox www.duckduckgo.com'
+sleep 1
+i3-msg 'workspace ; append_layout ~/.i3/firefox.json; exec firefox -new-window https://trello.com/b/gpLrkneq/machine-learning; workspace '
