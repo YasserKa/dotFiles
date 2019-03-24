a4198827ae1c710d18c0617184c35b823e52a25f
TUuno <31230143@students.liu.edu.lb>
2019-03-24 17:57:30 +0200

    binding CTRL-W CTRL-H for erasing in insert mode in qutebrowser

--- general/.config/qutebrowser/config.py
+++ general/.config/qutebrowser/config.py
@@ -8,7 +8,12 @@ config.bind('L', 'tab-next')
 config.bind('j', 'run-with-count 3 scroll down')
 config.bind('k', 'run-with-count 3 scroll up')
 
+config.bind('k', 'run-with-count 3 scroll up')
+
 config.bind(';', 'set-cmd-text :')
+
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='insert')
+config.bind('<Ctrl-h>', 'fake-key <backspace>', mode='insert')
 
 config.bind('p', 'open -t selection', mode='caret')
 config.set('scrolling.smooth', True)
