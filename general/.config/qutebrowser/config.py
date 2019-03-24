e2e5f22905b44e448a9ea2dd5324b47a85d29d6e
TUuno <31230143@students.liu.edu.lb>
2019-03-24 17:20:56 +0200

    updating the cursor sensitivity in qutebrowser

--- general/.config/qutebrowser/config.py
+++ general/.config/qutebrowser/config.py
@@ -5,6 +5,9 @@ config.bind('K', 'back')
 config.bind('H', 'tab-prev')
 config.bind('L', 'tab-next')
 
+config.bind('j', 'run-with-count 3 scroll down')
+config.bind('k', 'run-with-count 3 scroll up')
+
 config.bind(';', 'set-cmd-text :')
 
 config.bind('p', 'open -t selection', mode='caret')
