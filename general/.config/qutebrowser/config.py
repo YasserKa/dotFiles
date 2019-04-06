1f177180ba149fc4990cc3fb52c50058298123f0
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-04-06 11:52:05 +0300

    updating keybindings for qutebrowser

--- general/.config/qutebrowser/config.py
+++ general/.config/qutebrowser/config.py
@@ -11,22 +11,23 @@ config.bind('L', 'tab-next')
 config.bind('j', 'run-with-count 3 scroll down')
 config.bind('k', 'run-with-count 3 scroll up')
 
-config.bind('k', 'run-with-count 3 scroll up')
-
 config.bind(';', 'set-cmd-text :')
 
 config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='insert')
 config.bind('<Ctrl-h>', 'fake-key <backspace>', mode='insert')
 
-config.bind('<<', 'tab-move -')
-config.bind('>>', 'tab-move +')
+config.bind('<', 'tab-move -')
+config.bind('>', 'tab-move +')
 
 config.bind('o', 'open selection', mode='caret')
 config.bind('O', 'open -t selection', mode='caret')
 
+# open a tab in a new window then close it in the current window
+config.bind('wO', 'open -w {url} ;; tab-close')
+
+
 config.set('scrolling.bar', 'never')
 config.set('spellcheck.languages', ['en-US'])
-
 
 # doesn't work
 config.set('scrolling.smooth', True)
