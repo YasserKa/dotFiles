c9ee67cd42c39c455cfacde3bc6788f3bc70b095
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-04-06 11:51:31 +0300

    exchanging trello with vimwiki

--- i3/.i3/firefoxInit.sh
+++ i3/.i3/firefoxInit.sh
@@ -2,4 +2,4 @@
 
 i3-msg 'append_layout ~/.i3/firefox.json; exec firefox www.duckduckgo.com'
 sleep 1
-i3-msg 'workspace ; append_layout ~/.i3/firefox.json; exec firefox -new-window https://trello.com/b/gpLrkneq/machine-learning; workspace '
+# i3-msg 'workspace ; append_layout ~/.i3/firefox.json; exec firefox -new-window https://trello.com/b/gpLrkneq/machine-learning; workspace '
--- i3/.i3/qutebrowserInit.sh
+++ i3/.i3/qutebrowserInit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 i3-msg 'append_layout ~/.i3/qutebrowser.json; exec qutebrowser www.duckduckgo.com'
-sleep 1
-i3-msg 'workspace ; append_layout ~/.i3/qutebrowser.json; exec qutebrowser --target=window https://trello.com/b/gpLrkneq/machine-learning; workspace '
+# sleep 1
+# i3-msg 'workspace ; append_layout ~/.i3/qutebrowser.json; exec qutebrowser --target=window https://trello.com/b/AjB4OlPQ/general; workspace '
