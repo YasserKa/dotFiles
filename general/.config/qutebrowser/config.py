70876d2820a1935f56bc540ba545f611e2c57deb
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-04-18 17:22:37 +0300

    Qutebrowser: Cleaning+Adding `C-w` `C-h` functionality to passthrough

--- general/.config/qutebrowser/config.py
+++ general/.config/qutebrowser/config.py
@@ -1,33 +1,39 @@
-#  import config
+# navigation
+config.bind('j', 'run-with-count 3 scroll down')
+config.bind('k', 'run-with-count 3 scroll up')
+
 config.bind('J', 'forward')
 config.bind('K', 'back')
-
-config.bind('tJ', 'forward -t')
-config.bind('tK', 'back -t')
 
 config.bind('H', 'tab-prev')
 config.bind('L', 'tab-next')
 
-config.bind('j', 'run-with-count 3 scroll down')
-config.bind('k', 'run-with-count 3 scroll up')
-
-config.bind(';', 'set-cmd-text :')
-
-config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='insert')
-config.bind('<Ctrl-h>', 'fake-key <backspace>', mode='insert')
+config.bind('tJ', 'forward -t')
+config.bind('tK', 'back -t')
 
 config.bind('<', 'tab-move -')
 config.bind('>', 'tab-move +')
 
+config.bind(';', 'set-cmd-text :')
+
+# edit
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='insert')
+config.bind('<Ctrl-h>', 'fake-key <backspace>', mode='insert')
+
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='passthrough')
+config.bind('<Ctrl-h>', 'fake-key <backspace>', mode='passthrough')
+
 config.bind('o', 'open selection', mode='caret')
 config.bind('O', 'open -t selection', mode='caret')
+
+config.bind('<Ctrl-v>', 'leave-mode', mode='passthrough')
+
 
 # open a tab in a new window then close it in the current window
 config.bind('wO', 'open -w {url} ;; tab-close')
 
-
-config.set('scrolling.bar', 'never')
 config.set('spellcheck.languages', ['en-US'])
 
+config.set('scrolling.bar', 'never')
 # doesn't work
-config.set('scrolling.smooth', True)
+#  config.set('scrolling.smooth', True)
