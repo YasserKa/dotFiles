28ad0143eae40c28667ccb2f1ed8a84c1ceaefa5
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-05-22 05:20:43 +0300

    adding mangaComplete script

---
+++ general/bin/mangaComplete.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+manga_list = [
+        'https://myanimelist.net/manga/90125\
+                /Kaguya-sama_wa_Kokurasetai__Tensai-tachi_no_Renai_Zunousen',
+        'https://myanimelist.net/manga/44489/Houseki_no_Kuni',
+        'https://myanimelist.net/manga/40171\
+                /Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru',
+        'https://myanimelist.net/manga/91941/Made_in_Abyss',
+]
+
+finished_manga = []
+
+for manga in manga_list:
+    res = requests.get(manga)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    spans = soup.select('.js-scrollfix-bottom > .spaceit:contains("Finished")')
+    if len(spans) > 0:
+        finished_manga.append(manga)
+
+if len(finished_manga) > 0:
+    os.system('zenity \
+        --info --title finished manga \
+        --text ' + ''.join(finished_manga))
+    print(finished_manga)
