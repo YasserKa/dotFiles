dfbee46f9cb601b033cfe58646b471ef3125d0a2
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-05-29 19:16:22 +0300

    jupyter:  adding restart kernel + run cells above Ex commands

--- general/.jupyter/custom/custom.js
+++ general/.jupyter/custom/custom.js
@@ -14,7 +14,6 @@ require([
   // Add post callback
   vim_binding.on_ready_callbacks.push(function(){
     var km = ns.keyboard_manager;
-    // Allow Ctrl-2 to change the cell mode into Markdown in Vim normal mode
       km.edit_shortcuts.add_shortcut('ctrl-[', CodeMirror.prototype.leaveInsertMode, true);
       km.edit_shortcuts.add_shortcut('shift-ctrl-[', CodeMirror.prototype.leaveNormalMode, true);
       // Update Help
@@ -34,6 +33,16 @@ require([
           CodeMirror.Vim.handleKey(cm, ',', 'mapping');
       });
       CodeMirror.Vim.mapCommand("y", "action", "my_yank", {});
+      // restart-kernel-and-clear-ouput, run-all-cells-above
+
+      CodeMirror.Vim.defineEx('run-all-cells-above', 'run', function(cm) {
+          km.actions.call('jupyter-notebook:run-all-cells-above');
+          km.actions.call('jupyter-notebook:select-next-cell');
+          km.actions.call('jupyter-notebook:run-cell');
+      });
+      CodeMirror.Vim.defineEx('restart', 're', function(cm) {
+          km.actions.call('jupyter-notebook:restart-kernel-and-clear-output');
+      });
 
   });
 });
