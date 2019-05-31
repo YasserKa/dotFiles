3d80964ae14baf93db49490ba1edfa3cc26ad474
Yasser Kaddoura <yasser.kaddoura@augmental.education>
2019-05-31 19:41:40 +0300

    qutebrowser: changing ; to : for hint commands (eg. :I, :O, etc..)

--- general/.config/qutebrowser/config.py
+++ general/.config/qutebrowser/config.py
@@ -1,6 +1,7 @@
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
+# c.qt.args = ["ppapi-widevine-path=/home/yasser/qutebrowser/.venv/lib/python3.6/site-packages/PyQt5/Qt/plugins/ppapi/libwidevinecdm.so"]
 c.bindings.commands = {
         'normal': {
             'j': 'scroll-px 0 100',
@@ -21,36 +22,52 @@ c.bindings.commands = {
 
             ';': 'set-cmd-text :',
 
-            'es': 'config-edit',
-            'ss': 'config-source',
+            ':I': 'hint images tab',
+            ':O': 'hint links fill :open -t -r {hint-url}',
+            ':R': 'hint --rapid links window',
+            ':Y': 'hint links yank-primary',
+            ':b': 'hint all tab-bg',
+            ':d': 'hint links download',
+            ':f': 'hint all tab-fg',
+            ':h': 'hint all hover',
+            ':i': 'hint images',
+            ':o': 'hint links fill :open {hint-url}',
+            ':r': 'hint --rapid links tab-bg',
+            ':t': 'hint inputs',
+            ':y': 'hint links yank',
+
+            ',es': 'config-edit',
+            ',ss': 'config-source',
+            ',h': 'help -t',
 
             '<Ctrl-Shift-V>': 'enter-mode passthrough',
 
             'ye': 'spawn --userscript \
-            ~/.config/qutebrowser/userscripts/emacsMarkdown',
+                    ~/.config/qutebrowser/userscripts/emacsMarkdown',
             'yu': 'spawn --userscript \
-            ~/.config/qutebrowser/userscripts/youtube',
+                    ~/.config/qutebrowser/userscripts/youtube',
 
                             },
         'insert': {
-            '<Ctrl-w>': 'fake-key <Ctrl-backspace>',
-            '<Ctrl-h>': 'fake-key <backspace>',
+                '<Ctrl-w>': 'fake-key <Ctrl-backspace>',
+                '<Ctrl-h>': 'fake-key <backspace>',
 
-            '<Ctrl-p>': 'fake-key <Up>',
-            '<Ctrl-n>': 'fake-key <Down>',
-            },
+                '<Ctrl-p>': 'fake-key <Up>',
+                '<Ctrl-n>': 'fake-key <Down>',
+                },
         'caret': {
-            'o': 'open selection',
-            'O': 'open -t selection',
-            },
+                'o': 'open selection',
+                'O': 'open -t selection',
+                },
         'passthrough': {
-            '<Ctrl-w>': 'fake-key <Ctrl-backspace>',
-            '<Ctrl-h>': 'fake-key <backspace>',
+                '<Ctrl-w>': 'fake-key <Ctrl-backspace>',
+                '<Ctrl-h>': 'fake-key <backspace>',
 
-            '<Ctrl-Shift-V>': 'leave-mode',
-            },
+                '<Ctrl-Shift-V>': 'leave-mode',
+                },
         }
 config.unbind('<Ctrl-V>', mode='normal')
+config.unbind(':', mode='normal')
 
 config.set('spellcheck.languages', ['en-US'])
 
