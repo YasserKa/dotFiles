2b82ff09fdfa643a542639f9b62554c89e2959b4
TUuno <yasser.kaddoura@augmental.education>
2019-10-07 19:01:51 +0300

    removed youtube-dl from bin and updated manga list

--- general/bin/mangaComplete.py
+++ general/bin/mangaComplete.py
@@ -3,12 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 
 manga_list = [
-        'https://myanimelist.net/manga/90125\
-                /Kaguya-sama_wa_Kokurasetai__Tensai-tachi_no_Renai_Zunousen',
-        'https://myanimelist.net/manga/44489/Houseki_no_Kuni',
-        'https://myanimelist.net/manga/40171\
-                /Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru',
-        'https://myanimelist.net/manga/91941/Made_in_Abyss',
+        'https://myanimelist.net/manga/90125/\
+                Kaguya-sama_wa_Kokurasetai__Tensai-tachi_no_Renai_Zunousen',
+        'https://myanimelist.net/manga/48399/\
+                No_Game_No_Life',
+        'https://myanimelist.net/manga/44489/\
+                Houseki_no_Kuni',
+        'https://myanimelist.net/manga/40171/\
+                Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru',
+        'https://myanimelist.net/manga/91941/\
+                Made_in_Abyss',
+        'https://myanimelist.net/manga/74697/\
+                Re_Zero_kara_Hajimeru_Isekai_Seikatsu',
 ]
 
 finished_manga = []
