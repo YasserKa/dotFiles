920f8f141266f4a04fadb05e17480add8ba010a0
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2019-10-19 22:56:33 +0300

    moving and changing i3 config

--- i3/.i3/firefoxInit.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-i3-msg 'append_layout ~/.i3/firefox.json; exec firefox www.duckduckgo.com'
-sleep 1
-# i3-msg 'workspace ; append_layout ~/.i3/firefox.json; exec firefox -new-window https://trello.com/b/gpLrkneq/machine-learning; workspace '
