007de5794db27c951fdfde53246e512f2b0da431
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2019-11-03 18:18:15 +0200

    Battery: notify only if disacharging

--- general/bin/battery_low_alert.sh
+++ general/bin/battery_low_alert.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+is_charging=`acpi -a | grep 'on-line'`
 
-if [ $battery_level -le 10 ]
+if [ $battery_level -le 10 ] && [ -z "$is_charging" ]
 then
     dunstify "Battery low" "Battery level is ${battery_level}%!" --urgency=2
 fi
