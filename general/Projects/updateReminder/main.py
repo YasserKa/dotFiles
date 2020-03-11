9f8f16acc253ff1b6f53334b09c90a35efbd2bd6
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-03-11 22:10:53 +0200

    Adding cron file & update reminder script

---
+++ general/Projects/updateReminder/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+import configparser
+import smtplib
+
+config = configparser.ConfigParser()
+
+config.read_file(open('./config'))
+mail_pass = config.get('mail', 'password')
+from_mail = config.get('mail', 'mail')
+to_mail = config.get('mail', 'mail')
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_mail, mail_pass)
+
+    msg = """Subject: Updating reminder
+
+Making a monthly update for emacs, vim & pacman packages: macs
+
+- emacs package-list-packages <S-u> x
+        """
+
+    server.sendmail(from_mail, to_mail, msg)
+    server.quit()
+
+
+if __name__ == '__main__':
+    send_mail()
