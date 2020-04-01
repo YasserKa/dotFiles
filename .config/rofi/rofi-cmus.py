4110f45292fa0bd7191f4cf6cf3ec19451d6c1fb
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-04-01 14:46:58 +0300

    Scripts: Creating rofi-cmus and adding to i3 cmus mode

---
+++ .config/rofi/rofi-cmus.py
@@ -0,0 +1,28 @@
+#!/bin/python
+from os import environ
+import subprocess
+import re
+import sys
+
+CMUS_PATH_LIB = environ['XDG_CONFIG_HOME'] + '/cmus/lib.pl'
+MUSIC_PATH = environ['HOME'] + '/Music'
+
+
+def main():
+    if len(sys.argv) == 1:
+        # Show all the available music tracks
+        result = [re.match(r'^/(.+/)*(.+)\.(.+)$', line).group(2)
+                  for line in open(CMUS_PATH_LIB)]
+        print("\n".join(result))
+    else:
+        # Find the directory of the music track and play it
+        music_path = subprocess.getoutput(
+            'find ' + MUSIC_PATH + ' -name "' + re.escape(sys.argv[1])+'*"')
+
+        subprocess.run(
+            ['cmus-remote', '-C', 'add -q '+music_path])
+        subprocess.run(['cmus-remote', '-C', 'player-next'])
+
+
+if __name__ == "__main__":
+    main()
