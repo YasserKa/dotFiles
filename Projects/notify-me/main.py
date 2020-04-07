be38db19b71f6e9cf08238218d0121711df771ed
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-04-07 11:41:13 +0300

    scripts: notify-me, add riot submission

--- Projects/notify-me/main.py
+++ Projects/notify-me/main.py
@@ -5,14 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
-URL = 'https://discordapp.com/channels/@me'
+URL_DISCORD = 'https://discordapp.com/channels/@me'
+URL_SUBMISSION = 'https://developer.riotgames.com/app/427810/info'
 
-delay = 5
 executable_path = './chromedriver'
 profile_path = './myProfile'
 
 discord = ' Discord notification'
 reddit = ' Reddit notification'
+submission = 'Riot Submission'
 
 options = webdriver.ChromeOptions()
 
@@ -32,10 +33,27 @@ def check_discord():
     return notificaiton_exists
 
 
+def check_submission():
+
+    pending_el = driver.find_elements(
+        By.XPATH, './/span[contains(text(), "Pending")]')
+    pending_exists = len(pending_el) > 0
+
+    if pending_exists == 0:
+        subprocess.run(["dunstify", submission])
+
+    return pending_exists
+
+
 def main():
     try:
-        driver.get(URL)
+        driver.get(URL_DISCORD)
         WebDriverWait(driver, 10).until(lambda x: check_discord())
+    except TimeoutException:
+        pass
+    try:
+        driver.get(URL_SUBMISSION)
+        WebDriverWait(driver, 10).until(lambda x: check_submission())
     except TimeoutException:
         pass
 
