624d6b24d2009dd3890bb9552abf3a8bdc5eadb5
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-09-07 17:33:03 +0200

    Add: notify-me - add whatsapp

--- Projects/notify-me/main.py
+++ Projects/notify-me/main.py
@@ -7,19 +7,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 URL_DISCORD = 'https://discordapp.com/channels/@me'
-URL_SUBMISSION = 'https://developer.riotgames.com/app/427810/info'
+URL_WHATS_APP = 'https://web.whatsapp.com/'
 
 executable_path = './chromedriver'
 profile_path = './myProfile'
 
 discord = ' Discord notification'
 reddit = ' Reddit notification'
+whatsapp = 'Whatsapp notification'
 
 options = webdriver.ChromeOptions()
 
 options.add_argument('user-data-dir=' + profile_path)
 
-os.system("pkill chrom")
+os.system("pkill chromedrive")
 driver = webdriver.Chrome(executable_path=executable_path, options=options)
 
 
@@ -35,14 +36,27 @@ def check_discord():
     return notificaiton_exists
 
 
+def check_whatsapp():
+    unread_messages = driver.find_elements(
+        By.CSS_SELECTOR, 'span[aria-label*="unread message"]')
+    unread_messages_exist = len(unread_messages) > 0
+
+    if unread_messages_exist:
+        subprocess.run(["dunstify", whatsapp])
+
+    return unread_messages_exist
+
+
 def main():
     try:
-        driver.get(URL_DISCORD)
-        WebDriverWait(driver, 10).until(lambda x: check_discord())
+        # driver.get(URL_DISCORD)
+        # WebDriverWait(driver, 10).until(lambda x: check_discord())
+        driver.get(URL_WHATS_APP)
+        WebDriverWait(driver, 10).until(lambda x: check_whatsapp())
     except TimeoutException:
         pass
 
-    driver.close()
+    # driver.close()
 
 
 if __name__ == '__main__':
