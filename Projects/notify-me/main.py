d5466834a14d3bbbc960e98e62bc06d13846f0d3
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-09-10 23:23:17 +0200

    Fix: notify-me - wait until page loads

--- Projects/notify-me/main.py
+++ Projects/notify-me/main.py
@@ -20,11 +20,16 @@ options = webdriver.ChromeOptions()
 
 options.add_argument('user-data-dir=' + profile_path)
 
-os.system("pkill chromedrive")
+os.system("kill $(pgrep chromium --list-full | grep myProfile | cut -d ' ' -f1)")
+
 driver = webdriver.Chrome(executable_path=executable_path, options=options)
 
 
 def check_discord():
+
+    driver.get(URL_DISCORD)
+    WebDriverWait(driver, 10).until(
+        lambda x: len(x.find_elements_by_css_selector('div[aria-label="Download Apps"]')) > 0)
 
     notificaitons_el = driver.find_elements(
         By.CSS_SELECTOR, 'div[class*=lowerBadge-]')
@@ -33,30 +38,29 @@ def check_discord():
     if notificaiton_exists:
         subprocess.run(["dunstify", discord])
 
-    return notificaiton_exists
-
 
 def check_whatsapp():
+    driver.get(URL_WHATS_APP)
+    WebDriverWait(driver, 10).until(
+        lambda x: len(x.find_elements_by_xpath(
+            './/*[contains(text(),"Search or start new chat")]')) > 0)
+
     unread_messages = driver.find_elements(
-        By.CSS_SELECTOR, 'span[aria-label*="unread message"]')
+        By.CSS_SELECTOR, '._31gEB')
     unread_messages_exist = len(unread_messages) > 0
 
     if unread_messages_exist:
         subprocess.run(["dunstify", whatsapp])
 
-    return unread_messages_exist
-
 
 def main():
     try:
-        # driver.get(URL_DISCORD)
-        # WebDriverWait(driver, 10).until(lambda x: check_discord())
-        driver.get(URL_WHATS_APP)
-        WebDriverWait(driver, 10).until(lambda x: check_whatsapp())
+        check_discord()
+        check_whatsapp()
     except TimeoutException:
         pass
 
-    # driver.close()
+    driver.close()
 
 
 if __name__ == '__main__':
