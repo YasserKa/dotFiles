10445d61e870d12d3b56044a658865f0a3bac6a8
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2020-09-11 21:29:26 +0200

    New: polybar - multiple screen

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+# https://github.com/polybar/polybar/issues/763
+flock 200
 
- # Terminate already running bar instances
- killall -q polybar
 
- # Wait until the processes have been shut down
- while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+killall -q polybar
 
- # Launch Polybar, using default config location ~/.config/polybar/config
- polybar mybar &
+while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
- echo "Polybar launched..."
+outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+tray_output=eDP-1
+
+# for m in $outputs; do
+#     if [[ $m == "HDMI-1" ]]; then
+#         tray_output=$m
+#     fi
+# done
+
+for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+        TRAY_POSITION=right
+    fi
+
+    polybar --reload mybar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+done
