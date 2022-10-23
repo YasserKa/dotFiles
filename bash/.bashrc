950d3220fa9096cf99a7d203016f1e8c732e285f
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2022-10-23 18:51:49 +0200

    style(bash): change indentation length

--- bash/.bashrc
+++ bash/.bashrc
@@ -2,100 +2,104 @@
 # Source if the shell is interactive
 # shellcheck disable=SC1091,SC2034,SC1090,SC2155,SC2016
 case "$-" in
-    *i*) ;;
-    *) return ;;
+*i*) ;;
+*) return ;;
 esac
 
 # Bash options https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
-set -o vi                  # Vi mode
-set -o noclobber           # Don't overwrite when redireting using shell
-shopt -s autocd            # Auto cd without using cd
-shopt -s histappend        # Append to the history file, don't overwrite it
-shopt -s cdspell           # Fix spelling errors when using cd
-shopt -s dirspell          # Fix spelling errors during tab-completion
-shopt -s extglob           # extend pattern matching
-shopt -s cmdhist           # History records commands with multiple lines as such instead of using ";"
-stty -ixon                 # Enable search C-s for history searching (Enable XON/XOFF flow control)
+set -o vi           # Vi mode
+set -o noclobber    # Don't overwrite when redireting using shell
+shopt -s autocd     # Auto cd without using cd
+shopt -s histappend # Append to the history file, don't overwrite it
+shopt -s cdspell    # Fix spelling errors when using cd
+shopt -s dirspell   # Fix spelling errors during tab-completion
+shopt -s extglob    # extend pattern matching
+shopt -s cmdhist    # History records commands with multiple lines as such instead of using ";"
+stty -ixon          # Enable search C-s for history searching (Enable XON/XOFF flow control)
+
+source_file() {
+	[[ -f "$1" ]] && . "$1"
+}
 
 # Complete command names and file names for super user
 complete -cf sudo
-[[ -f /usr/share/doc/pkgfile/command-not-found.bash ]] && . /usr/share/doc/pkgfile/command-not-found.bash
+source_file /usr/share/doc/pkgfile/command-not-found.bash
 # Gives autocompletion for options
 # https://github.com/scop/bash-completion
-[[ -f /usr/share/bash-completion/bash_completion ]] && . /usr/share/bash-completion/bash_completion
+source_file /usr/share/bash-completion/bash_completion
 
-[[ -f "$XDG_CONFIG_HOME/tmux/tmux_completion" ]] && . "$XDG_CONFIG_HOME/tmux/tmux_completion"
+source_file "$XDG_CONFIG_HOME/tmux/tmux_completion"
 
 # Prompt with info about git repo
 if [[ -e $XDG_CONFIG_HOME/git/git-prompt.sh ]]; then
-    # https://www.git-scm.com/book/en/v2/Appendix-A%3A-Git-in-Other-Environments-Git-in-Bash
-    . "$XDG_CONFIG_HOME/git/git-prompt.sh"
-    export GIT_PS1_SHOWDIRTYSTATE=1
-    PS1='\[$(tput bold)\]\[\e[36m\]\w\[\033[38;5;87m\] $(__git_ps1 "(%s)")\n\[\033[38;5;34m\]❯ \[\e[m\]'
+	# https://www.git-scm.com/book/en/v2/Appendix-A%3A-Git-in-Other-Environments-Git-in-Bash
+	. "$XDG_CONFIG_HOME/git/git-prompt.sh"
+	export GIT_PS1_SHOWDIRTYSTATE=1
+	PS1='\[$(tput bold)\]\[\e[36m\]\w\[\033[38;5;87m\] $(__git_ps1 "(%s)")\n\[\033[38;5;34m\]❯ \[\e[m\]'
 else
-    git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1)/'; }
-    PS1='\[$(tput bold)\]\[\e[36m\]\w\[\033[38;5;87m\] $(git_branch)\n\[\033[38;5;34m\]❯ \[\e[m\]'
+	git_branch() { git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1)/'; }
+	PS1='\[$(tput bold)\]\[\e[36m\]\w\[\033[38;5;87m\] $(git_branch)\n\[\033[38;5;34m\]❯ \[\e[m\]'
 fi
 
 # Setup autojumping
 # https://github.com/clvv/fasd
-if command -v fasd > /dev/null; then
-    fasd_cache="$HOME/.fasd-init-bash"
-    # Cache the code for faster sourcing time
-    if [[ ! -s "$fasd_cache" ]]; then
-        fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
-    fi
-    . "$fasd_cache"
-    unset -v fasd_cache
-    alias vf='fasd -sife nvim' # quick opening files with vim
+if command -v fasd >/dev/null; then
+	fasd_cache="$HOME/.fasd-init-bash"
+	# Cache the code for faster sourcing time
+	if [[ ! -s "$fasd_cache" ]]; then
+		fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >|"$fasd_cache"
+	fi
+	. "$fasd_cache"
+	unset -v fasd_cache
+	alias vf='fasd -sife nvim' # quick opening files with vim
 
-    _fasd_bash_hook_cmd_complete vf j
+	_fasd_bash_hook_cmd_complete vf j
 fi
 
 # Commands with colors
 # https://github.com/garabik/grc
-if command -v grc > /dev/null; then
-    GRC_ALIASES=true
-    [[ -x /etc/profile.d/grc.sh ]] && . /etc/profile.d/grc.sh
+if command -v grc >/dev/null; then
+	GRC_ALIASES=true
+	[[ -x /etc/profile.d/grc.sh ]] && . /etc/profile.d/grc.sh
 fi
 
-if command -v fzf > /dev/null; then
-    [[ -f /usr/share/fzf/completion.bash ]] && . /usr/share/fzf/completion.bash
-    [[ -f /usr/share/fzf/key-bindings.bash ]] && . /usr/share/fzf/key-bindings.bash
+if command -v fzf >/dev/null; then
+	source_file /usr/share/fzf/completion.bash
+	source_file /usr/share/fzf/key-bindings.bash
 
-    # For C-r history search, make <tab> edit selected option and <enter> execute it
-    FZF_CTRL_R_EDIT_KEY=ctrl-e
-    FZF_CTRL_R_EXEC_KEY=enter
-    [[ -f $XDG_CONFIG_HOME/fzf/history-exec.bash ]] && . "$XDG_CONFIG_HOME/fzf/history-exec.bash"
+	# For C-r history search, make <tab> edit selected option and <enter> execute it
+	FZF_CTRL_R_EDIT_KEY=ctrl-e
+	FZF_CTRL_R_EXEC_KEY=enter
+	source_file "$XDG_CONFIG_HOME/fzf/history-exec.bash"
 
-    # Taken from FZF's key-bindings.bash to make C-t works
-    # history-exec.bash seems to make the binding not work
-    bind -m vi-insert -x '"\C-t": fzf-file-widget'
+	# Taken from FZF's key-bindings.bash to make C-t works
+	# history-exec.bash seems to make the binding not work
+	bind -m vi-insert -x '"\C-t": fzf-file-widget'
 fi
 
 # Readline with autopairs
-[[ -f "$XDG_CONFIG_HOME/readline/autopairs" ]] && . "$XDG_CONFIG_HOME/readline/autopairs"
+source_file "$XDG_CONFIG_HOME/readline/autopairs"
 
 # Enable tmux pane & vim windows navigation
 # If terminal is in vi normal mode, trigger navigation, otherwise use C-*
 # normally
 if [[ -n "${TMUX}" ]]; then
-    is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-    bind -m vi-command -x '"\C-h": tmux if-shell "$is_vim" "send-keys C-h"  "select-pane -L"'
-    bind -m vi-command -x '"\C-j": tmux if-shell "$is_vim" "send-keys C-j"  "select-pane -D"'
-    bind -m vi-command -x '"\C-k": tmux if-shell "$is_vim" "send-keys C-k"  "select-pane -U"'
-    bind -m vi-command -x '"\C-l": tmux if-shell "$is_vim" "send-keys C-l"  "select-pane -R"'
+	is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+	bind -m vi-command -x '"\C-h": tmux if-shell "$is_vim" "send-keys C-h"  "select-pane -L"'
+	bind -m vi-command -x '"\C-j": tmux if-shell "$is_vim" "send-keys C-j"  "select-pane -D"'
+	bind -m vi-command -x '"\C-k": tmux if-shell "$is_vim" "send-keys C-k"  "select-pane -U"'
+	bind -m vi-command -x '"\C-l": tmux if-shell "$is_vim" "send-keys C-l"  "select-pane -R"'
 fi
 
 # Source files
-for bash in "$HOME"/.bashrc.d/*.bash ; do
-    . "$bash"
+for bash in "$HOME"/.bashrc.d/*.bash; do
+	. "$bash"
 done
 unset -v bash
 
 # Files with colors
 # https://github.com/trapd00r/LS_COLORS
-[[ -f /usr/share/LS_COLORS/dircolors.sh ]] && . /usr/share/LS_COLORS/dircolors.sh
+source_file /usr/share/LS_COLORS/dircolors.sh
 
 # Make sure that pinentry uses the correct TTY
 export GPG_TTY=$(tty)
--- bash/.bashrc.d/functions.bash
+++ bash/.bashrc.d/functions.bash
@@ -2,63 +2,65 @@
 
 # Extract compressed files function
 extract() {
-    local c e i
+	local c e i
 
-    (($#)) || return
+	(($#)) || return
 
-    for i; do
-        c=''
-        e=1
+	for i; do
+		c=''
+		e=1
 
-        if [[ ! -r $i ]]; then
-            echo "$0: file is unreadable: \`$i'" >&2
-            continue
-        fi
+		if [[ ! -r $i ]]; then
+			echo "$0: file is unreadable: \`$i'" >&2
+			continue
+		fi
 
-        case $i in
-            *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz))))) c=(bsdtar xvf);;
-            *.7z)  c=(7z x);;
-            *.Z)   c=(uncompress);;
-            *.bz2) c=(bunzip2);;
-            *.exe) c=(cabextract);;
-            *.gz)  c=(gunzip);;
-            *.rar) c=(unrar x);;
-            *.xz)  c=(unxz);;
-            *.zip) c=(unzip);;
-            *.zst) c=(unzstd);;
-            *)     echo "$0: unrecognized file extension: \`$i'" >&2
-                continue;;
-        esac
+		case $i in
+		*.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz))))) c=(bsdtar xvf) ;;
+		*.7z) c=(7z x) ;;
+		*.Z) c=(uncompress) ;;
+		*.bz2) c=(bunzip2) ;;
+		*.exe) c=(cabextract) ;;
+		*.gz) c=(gunzip) ;;
+		*.rar) c=(unrar x) ;;
+		*.xz) c=(unxz) ;;
+		*.zip) c=(unzip) ;;
+		*.zst) c=(unzstd) ;;
+		*)
+			echo "$0: unrecognized file extension: \`$i'" >&2
+			continue
+			;;
+		esac
 
-        command "${c[@]}" "$i"
-        ((e = e || $?))
-    done
-    return "$e"
+		command "${c[@]}" "$i"
+		((e = e || $?))
+	done
+	return "$e"
 }
 
 # Remove dependencies that are no longer needed (orphans)
 orphans() {
-    if [[ $(pacman -Qdtt) ]]; then
-        echo "no orphans to remove"
-    else
-        sudo pacman -Rnsc "$(pacman -Qdttq)"
-    fi
+	if [[ $(pacman -Qdtt) ]]; then
+		echo "no orphans to remove"
+	else
+		sudo pacman -Rnsc "$(pacman -Qdttq)"
+	fi
 }
 
 upgrade_system() {
-    orphans
-    paru --sync --refresh --sysupgrade --noconfirm
-    printf "%s\n" "Updating Vim packages"
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-    printf "%s\n" "Updating Emacs packages"
-    # Update packages and exit afterwards
-    emacs --no-window-system --eval "(progn
+	orphans
+	paru --sync --refresh --sysupgrade --noconfirm
+	printf "%s\n" "Updating Vim packages"
+	nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+	printf "%s\n" "Updating Emacs packages"
+	# Update packages and exit afterwards
+	emacs --no-window-system --eval "(progn
         (add-hook 'emacs-startup-hook #'(lambda () (interactive) (save-buffers-kill-emacs)))
         (auto-packages-update-now))"
 
-    make --file="${HOME}/dotfiles/Makefile" upgrade-pypi-packages
+	make --file="${HOME}/dotfiles/Makefile" upgrade-pypi-packages
 
-    echo 'Use the following commands to checkup on the system:
+	echo 'Use the following commands to checkup on the system:
     systemctl --failed --user
     logxorg
     sudo journalctl -p 3 -b'
@@ -67,82 +69,86 @@ upgrade_system() {
 # Automatically change current directory to the last visited one after ranger quits
 # /usr/share/doc/ranger/examples/shell_automatic_cd.sh
 ranger() {
-    temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
-    command ranger --choosedir="$temp_file" -- "${@:-$PWD}"
-    if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
-        cd -- "$chosen_dir" || exit
-    fi
-    rm -f -- "$temp_file"
+	temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
+	command ranger --choosedir="$temp_file" -- "${@:-$PWD}"
+	if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+		cd -- "$chosen_dir" || exit
+	fi
+	rm -f -- "$temp_file"
 }
 
 # Use fasd and FZF to jump through directories
 j() {
-    paths=$(fasd -dlR "$@")
-    path="$HOME"
-    # If only one path exists, go to it
-    if [[ $(echo -e "$paths" | wc -l) == 1 ]]; then
-        path=$paths
-    else # Use fzf otherwise
-        path=$(echo -e "$paths" | fzf --preview-window hidden --keep-right --height=20 --layout=reverse)
-    fi
-    cd "$path" || exit 1
+	paths=$(fasd -dlR "$@")
+	path="$HOME"
+	# If only one path exists, go to it
+	if [[ $(echo -e "$paths" | wc -l) == 1 ]]; then
+		path=$paths
+	else # Use fzf otherwise
+		path=$(echo -e "$paths" | fzf --preview-window hidden --keep-right --height=20 --layout=reverse)
+	fi
+	cd "$path" || exit 1
 }
 
 # Pick tmux sessions using FZF
 fzftmux() {
-    TMUX_SESSION=$(tmux list-sessions | cut -d: -f1 | fzf)
-    [[ -z $TMUX_SESSION ]] && return
-    tmux attach-session -t "$TMUX_SESSION"
+	TMUX_SESSION=$(tmux list-sessions | cut -d: -f1 | fzf)
+	[[ -z $TMUX_SESSION ]] && return
+	tmux attach-session -t "$TMUX_SESSION"
 }
 
 # Edit last n lines in history using $EDITOR
 fclast() { command fc "-${1}" 0; }
 
 # Copy last command
-cl() { history -p '!!'| tr -d \\n | pbcopy &>/dev/null; }
+cl() { history -p '!!' | tr -d \\n | pbcopy &>/dev/null; }
 
 copyline() { printf %s "$READLINE_LINE" | pbcopy &>/dev/null; }
-bind -x '"\C-y":copyline'
+bind -m vi-insert -x '"\C-y":copyline'
+bind -m vi-command -x '"\C-y":copyline'
 
 # Use Alt-h to view documentation for commands
 run_help() {
-    local -r cmd="$READLINE_LINE"
-    help "$cmd" 2>/dev/null || man "$cmd" 2>/dev/null || $cmd --help | $PAGER
+	local -r cmd="$READLINE_LINE"
+	help "$cmd" 2>/dev/null || man "$cmd" 2>/dev/null || $cmd --help | $PAGER
 }
 bind -m vi-insert -x '"\eh": run_help'
 
 # Attach job & send notification after it's finished
-alert_last() { fg; notify-send --expire-time=99999 "$(history | tail -n 2 | head -n 1 | cut -d ' ' -f 3-)"; }
+alert_last() {
+	fg
+	notify-send --expire-time=99999 "$(history | tail -n 2 | head -n 1 | cut -d ' ' -f 3-)"
+}
 
 # Open TUIR apps from menu picker (spawning a termianl) or the command line
 open_cli() {
-    local command="$1"
+	local command="$1"
 
-    [[ ! $(command -v "$command") ]] &&
-        notify-send "$command doesn't exit" && return 127
+	[[ ! $(command -v "$command") ]] &&
+		notify-send "$command doesn't exit" && return 127
 
-    [[ $TERMINAL != "kitty" && $TERMINAL != "alacritty" ]] && notify-send "$TERMINAL is not supported" && return 1
+	[[ $TERMINAL != "kitty" && $TERMINAL != "alacritty" ]] && notify-send "$TERMINAL is not supported" && return 1
 
-    # Command is run from a shell using -c option
-    if [[ "$-" != *c* ]]; then
-        command "$command"
-    else
-        $TERMINAL --detach -e bash -c "$command && exec bash"
-    fi
+	# Command is run from a shell using -c option
+	if [[ "$-" != *c* ]]; then
+		command "$command"
+	else
+		$TERMINAL --detach -e bash -c "$command && exec bash"
+	fi
 }
 
 cli_list=("newsboat" "neomutt")
 
 for cli in "${cli_list[@]}"; do
-    # shellcheck disable=SC2139
-    alias "$cli=open_cli $cli"
+	# shellcheck disable=SC2139
+	alias "$cli=open_cli $cli"
 done
 unset cli
 
 # Open TUIR with top page within 24 hours by pressing "g t 2"
 tuir() {
-    (xdotool search --sync --name "^Front Page - tuir" key g t 2 &)
-    open_cli tuir
+	(xdotool search --sync --name "^Front Page - tuir" key g t 2 &)
+	open_cli tuir
 }
 
 ################################################################################
@@ -152,116 +158,119 @@ tuir() {
 #     $@: file name
 ################################################################################
 open_file() {
-    [[ $TERMINAL != "kitty" && $TERMINAL != "alacritty" ]] &&
-        notify-send "$TERMINAL not supported for open_file function" &&
-        return 1
+	[[ $TERMINAL != "kitty" && $TERMINAL != "alacritty" ]] &&
+		notify-send "$TERMINAL not supported for open_file function" &&
+		return 1
 
-    local -r DIRECTORY_PATH="$1"
-    shift
-    [[ ! -d "${DIRECTORY_PATH}" ]] && notify-send "${DIRECTORY_PATH} doesn't exist" && exit 1
+	local -r DIRECTORY_PATH="$1"
+	shift
+	[[ ! -d "${DIRECTORY_PATH}" ]] && notify-send "${DIRECTORY_PATH} doesn't exist" && exit 1
 
-    # Check if it's in a terminal
-    if [[ "$-" != *c* ]]; then
-        # The -o +only arguments are a hack to mitigate nvim's warning upon
-        # exiting for editing multiple files
-        # -o open files in windows and +only keep one of them
-        cd "${DIRECTORY_PATH}" && "${EDITOR}" +only -o "$@"
-    else
-        cd "${DIRECTORY_PATH}" && "${_EDITOR_GUI}" "$@" -- -o +only
-    fi
+	# Check if it's in a terminal
+	if [[ "$-" != *c* ]]; then
+		# The -o +only arguments are a hack to mitigate nvim's warning upon
+		# exiting for editing multiple files
+		# -o open files in windows and +only keep one of them
+		# shellcheck disable=SC2046,SC2116
+		cd "${DIRECTORY_PATH}" && "${EDITOR}" +only -o $(echo "$@")
+	else
+		# shellcheck disable=SC2046,SC2116
+		"${TERMINAL}" --directory "${DIRECTORY_PATH}" --detach -e "${EDITOR}" +only -o "$@"
+	fi
 }
 
-alias rcreadline='open_file $XDG_CONFIG_HOME/readline inputrc'
-alias rcgpg='open_file $GNUPGHOME gpg-agent.conf'
-alias rcssh='open_file $HOME ssh sshd_config'
-alias rcx11='open_file ${XINITRC%/*} xinitrc'
-alias rcbash='open_file $HOME .bashrc .bash_profile .profile .bashrc.d/*bash'
-alias rckitty='open_file $XDG_CONFIG_HOME/kitty kitty.conf'
-alias rcvim='open_file $XDG_CONFIG_HOME/nvim init.vim'
-alias rci3='open_file $XDG_CONFIG_HOME/i3 config'
-alias rcneomutt='open_file $XDG_CONFIG_HOME/neomutt neomuttrc'
-alias rctuir='open_file $XDG_CONFIG_HOME/tuir tuir.cfg'
-alias rcnewsboat='open_file $XDG_CONFIG_HOME/newsboat'
-alias rcfeh='open_file $XDG_CONFIG_HOME/feh keys'
-alias rcrofi='open_file $XDG_CONFIG_HOME/rofi config.rasi'
-alias rcdunst='open_file $XDG_CONFIG_HOME/dunst dunstrc'
-alias rcpolybar='open_file $XDG_CONFIG_HOME/polybar config.ini'
-alias rctmux='open_file $XDG_CONFIG_HOME/tmux tmux.conf'
-alias rczathura='open_file $XDG_CONFIG_HOME/zathura zathurarc'
-alias rcqutebrowser='open_file $XDG_CONFIG_HOME/qutebrowser config.py'
+alias rcreadline='open_file $XDG_CONFIG_HOME/readline inputrc $XDG_CONFIG_HOME/readline/*'
+alias rcgpg='open_file $GNUPGHOME gpg-agent.conf $GNUPGHOME/*'
+alias rcssh='open_file $HOME/.ssh config $HOME/.ssh*'
+alias rcx11='open_file ${XINITRC%/*} xinitrc ${XINITRC%/*}/*'
+alias rcbash='open_file $HOME/.bashrc.d ../.bashrc ../.bash_profile ../.profile $HOME/.bashrc.d/*'
+alias rckitty='open_file $XDG_CONFIG_HOME/kitty kitty.conf $XDG_CONFIG_HOME/kitty/*'
+alias rcvim='open_file $XDG_CONFIG_HOME/nvim ./lua/user/init.lua'
+alias rci3='open_file $XDG_CONFIG_HOME/i3 config $XDG_CONFIG_HOME/i3/*'
+alias rcneomutt='open_file $XDG_CONFIG_HOME/neomutt neomuttrc $XDG_CONFIG_HOME/neomutt/*'
+alias rcmutt='open_file $XDG_CONFIG_HOME/neomutt neomuttrc $XDG_CONFIG_HOME/neomutt/*'
+alias rctuir='open_file $XDG_CONFIG_HOME/tuir tuir.cfg $XDG_CONFIG_HOME/tuir/*'
+alias rcnewsboat='open_file $XDG_CONFIG_HOME/newsboat $XDG_CONFIG_HOME/newsboat/*'
+alias rcfeh='open_file $XDG_CONFIG_HOME/feh keys $XDG_CONFIG_HOME/feh/*'
+alias rcrofi='open_file $XDG_CONFIG_HOME/rofi config.rasi $XDG_CONFIG_HOME/rofi/*'
+alias rcdunst='open_file $XDG_CONFIG_HOME/dunst dunstrc $XDG_CONFIG_HOME/dunst/*'
+alias rcpolybar='open_file $XDG_CONFIG_HOME/polybar config.ini $XDG_CONFIG_HOME/polybar/*'
+alias rctmux='open_file $XDG_CONFIG_HOME/tmux tmux.conf $XDG_CONFIG_HOME/tmux/*'
+alias rczathura='open_file $XDG_CONFIG_HOME/zathura zathurarc $XDG_CONFIG_HOME/zathura/*'
+alias rcqutebrowser='open_file $XDG_CONFIG_HOME/qutebrowser config.py $XDG_CONFIG_HOME/qutebrowser/userscripts/*'
 
 # Open Emacs's config file in Emacs
 alias rcemacs='emacs --file $XDG_CONFIG_HOME/emacs/init.el'
 
 rcdotfiles() {
-    if [[ "$-" != *c* ]]; then
-        cd "${HOME}/dotfiles/" || return
-    else
-        "${TERMINAL}" -e --directory "${HOME}/dotfiles/"
-    fi
+	if [[ "$-" != *c* ]]; then
+		cd "${HOME}/dotfiles/" || return
+	else
+		"${TERMINAL}" -e --directory "${HOME}/dotfiles/"
+	fi
 }
 alias cron='open_file $XDG_CONFIG_HOME/cron crons.cron; crontab $XDG_CONFIG_HOME/cron/crons.cron'
 
 open_gui() {
-    local name="$1"
-    local command="$2"
+	local name="$1"
+	local command="$2"
 
-    if ! xdotool search --name "$name" windowactivate; then
-        bash -c "chronic ${command} & disown"
+	if ! xdotool search --name "$name" windowactivate; then
+		bash -c "chronic ${command} & disown"
 
-        # --sync doesn't seem to work, so keep activating until it works
-        while [[ "$(xdotool getactivewindow getwindowname)" != "$name" ]]; do
-            xdotool search --sync --name "$name" windowactivate
-        done
-    fi
+		# --sync doesn't seem to work, so keep activating until it works
+		while [[ "$(xdotool getactivewindow getwindowname)" != "$name" ]]; do
+			xdotool search --sync --name "$name" windowactivate
+		done
+	fi
 }
 
 org() {
-    local name="emacs_org_name"
+	local name="emacs_org_name"
 
-    open_gui $name "emacs --title=$name --file=$_NOTES_ORG_HOME/capture.org"
+	open_gui $name "emacs --title=$name --file=$_NOTES_ORG_HOME/capture.org"
 }
 
 magit() {
-    local name="magit_name"
-    local git_root
-    git_root=$(git rev-parse --show-toplevel)
+	local name="magit_name"
+	local git_root
+	git_root=$(git rev-parse --show-toplevel)
 
-    chronic git rev-parse --show-toplevel || return 1
-    open_gui $name "emacs --title=$name --eval '(magit-status \"${git_root}\")'"
+	chronic git rev-parse --show-toplevel || return 1
+	open_gui $name "emacs --title=$name --eval '(magit-status \"${git_root}\")'"
 }
 
 alias gitdotfiles='cd $HOME/dotfiles && magit'
 
 syncorg() {
-    emacsclient --no-wait --eval "(org-save-all-org-buffers)"
-    "$HOME"/bin/wait_internet && rclone sync "${_NOTES_ORG_HOME}" org_notes:org --include 'fast_access.org' --include 'groceries.org'
+	emacsclient --no-wait --eval "(org-save-all-org-buffers)"
+	"$HOME"/bin/wait_internet && rclone sync "${_NOTES_ORG_HOME}" org_notes:org --include 'fast_access.org' --include 'groceries.org'
 }
 
 reboot() {
-    syncorg
-    command shutdown --reboot now
+	syncorg
+	command shutdown --reboot now
 }
 
 shutdown() {
-    syncorg
-    command shutdown now
+	syncorg
+	command shutdown now
 }
 
 # Which is unreliable, use type -P instead
 # https://unix.stackexchange.com/questions/85249/why-not-use-which-what-to-use-then
 which() {
-    printf >&2 'The which command is unreliable. Use type -P %s\n' "$*"
-    return 2
+	printf >&2 'The which command is unreliable. Use type -P %s\n' "$*"
+	return 2
 }
 
 # Reload qutebrowser and return to active window
 reload_browser() {
-    WINDOW_NAME="$1"
-    FOCUSED_ID="$(xdotool getwindowfocus)"
-    xdotool search --onlyvisible --name "${WINDOW_NAME}" windowfocus key --delay 100 --window %@ 'r'
-    sleep 0.5
-    xdotool windowfocus "${FOCUSED_ID}"
+	WINDOW_NAME="$1"
+	FOCUSED_ID="$(xdotool getwindowfocus)"
+	xdotool search --onlyvisible --name "${WINDOW_NAME}" windowfocus key --delay 100 --window %@ 'r'
+	sleep 0.5
+	xdotool windowfocus "${FOCUSED_ID}"
 }
 
 # Commands run in background automatically
