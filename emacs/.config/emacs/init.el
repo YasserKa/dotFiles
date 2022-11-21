acc60965df3891fe06732f7b4544e339ab09f6e4
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2022-11-21 18:22:08 +0100

    feat(emacs): add icons to citar while selecting a paper

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1041,6 +1041,12 @@ see how ARG affects this command."
   (citar-bibliography org-cite-global-bibliography)
   ;; Open files using external program
   (citar-file-open-functions (list (cons t 'citar-file-open-external)))
+  ;; Icons
+  (setq citar-symbols
+        `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+          (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+          (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+  (setq citar-symbol-separator "  ")
   :bind )
 
 ;; Show emphasis markers when hovering over text
