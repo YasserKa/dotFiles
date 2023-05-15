600944f2772a26cf21c5c27500172f1e4e36a256
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2023-05-15 21:29:25 +0200

    fix(vim): fix null-ls formatting

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -628,7 +628,7 @@ local config = {
 		{ "https://github.com/honza/vim-snippets", lazy = false },
 		{
 			"jose-elias-alvarez/null-ls.nvim",
-			opts = function(config) -- overrides `require("null-ls").setup(config)`
+			opts = function(_,config) -- overrides `require("null-ls").setup(config)`
 				-- config variable is the default configuration table for the setup function call
 				local null_ls = require("null-ls")
 				local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -655,21 +655,7 @@ local config = {
 					-- null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.prettier,
 				}
-				-- you can reuse a shared lspconfig on_attach callback here
-				config.on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end
-				return config
+ 			return config
 			end,
 		},
 		{
