18b5621ca419e05a7c2dcbf8a9a1d6e12a44657e
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2024-05-01 11:46:35 +0200

    feat(vim): add restore session feature based on dir

--- nvim/.config/nvim/lua/plugins/astrocore_sessions.lua
+++ nvim/.config/nvim/lua/plugins/astrocore_sessions.lua
@@ -2,18 +2,22 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configuration table of session options for AstroNvim's session management powered by Resession
-    sessions = {
-      -- Configure auto saving
-      autosave = {
-        last = true, -- auto save last session
-        cwd = true, -- auto save session for each working directory
-      },
-      -- Patterns to ignore when saving sessions
-      ignore = {
-        dirs = {}, -- working directories to ignore sessions in
-        filetypes = { "gitcommit", "gitrebase" }, -- filetypes to ignore sessions
-        buftypes = {}, -- buffer types to ignore sessions
+    autocmds = {
+      -- disable alpha autostart
+      alpha_autostart = false,
+      restore_session = {
+        {
+          event = "VimEnter",
+          desc = "Restore previous directory session if neovim opened with no arguments",
+          nested = true, -- trigger other autocommands as buffers open
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+              -- try to load a directory session using the current working directory
+              require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+            end
+          end,
+        },
       },
     },
   },
