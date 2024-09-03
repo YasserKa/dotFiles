2c5a050ba77ca230fecf34b0ca879c83d7482d47
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2024-09-03 13:39:55 +0300

    refactor(qutebrowser)

--- qutebrowser/.config/qutebrowser/userscripts/code_select.py
+++ qutebrowser/.config/qutebrowser/userscripts/code_select.py
@@ -5,6 +5,7 @@ import html
 import re
 import sys
 import xml.etree.ElementTree as ET
+
 try:
     import pyperclip
 except ImportError:
@@ -20,10 +21,10 @@ else:
 
 def parse_text_content(element):
     # https://stackoverflow.com/a/35591507/15245191
-    magic = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    magic = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
                 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" [
                 <!ENTITY nbsp ' '>
-                ]>'''
+                ]>"""
     root = ET.fromstring(magic + element)
     text = ET.tostring(root, encoding="unicode", method="text")
     text = html.unescape(text)
@@ -41,14 +42,13 @@ def main():
     # https://github.com/qutebrowser/qutebrowser/blob/master/doc/userscripts.asciidoc
     element = os.environ.get("QUTE_SELECTED_HTML")
     code_text = parse_text_content(element)
-    re_remove_dollars = re.compile(r"^(\$ )", re.MULTILINE)
-    code_text = re.sub(re_remove_dollars, '', code_text)
+    code_text = re.sub(r"^(\$ )", "", code_text, flags=re.MULTILINE)
     if PYPERCLIP:
         pyperclip.copy(code_text)
         send_command_to_qute(
             "message-info 'copied to clipboard: {info}{suffix}'".format(
-                info=code_text.splitlines()[0].replace("'", "\""),
-                suffix="..." if len(code_text.splitlines()) > 1 else ""
+                info=code_text.splitlines()[0].replace("'", '"'),
+                suffix="..." if len(code_text.splitlines()) > 1 else "",
             )
         )
     else:
@@ -56,7 +56,7 @@ def main():
         # compromise by placing lines on a single line seperated by the
         # specified delimiter
         code_text = re.sub("(\n)+", delimiter, code_text)
-        code_text = code_text.replace("'", "\"")
+        code_text = code_text.replace("'", '"')
         send_command_to_qute("yank inline '{code}'\n".format(code=code_text))
 
 
