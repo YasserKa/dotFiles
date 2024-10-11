edad83df488c3923fc10333a9128f09e5813f7b5
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2024-10-11 08:26:41 +0300

    feat(i3): add autotiling

--- i3/.config/i3/i3-center-cursor-on-window.sh
+++ i3/.config/i3/i3-center-cursor-on-window.sh
@@ -2,13 +2,30 @@
 #
 # Centers the cursor to the center of window after navigating and moving windows in i3
 # Dependencies xdotool i3
-
-eval i3-msg "$*"
+#
+# Provides WINDOW's PID, X start, Y start, WIDTH, HEIGHT
 
 CURR_WINDOW_ID=$(xdotool getwindowfocus)
-
-# Provides WINDOW's PID, X start, Y start, WIDTH, HEIGHT
 eval "$(xdotool getwindowgeometry --shell "$CURR_WINDOW_ID")"
+CURR_X="$X"
+CURR_Y="$Y"
+CURR_WIDTH="$WIDTH"
+CURR_HEIGHT="$HEIGHT"
+
+i3-msg "$*"
+
+if [[ "$1" == "focus" ]]; then
+  CURR_WINDOW_ID=$(xdotool getwindowfocus)
+fi
+eval "$(xdotool getwindowgeometry --shell "$CURR_WINDOW_ID")"
+
+i=0
+while ((i<=5)) && [[ "$1" == "move" ]] && ((CURR_X==X)) && ((CURR_Y==Y)) && ((CURR_WIDTH==WIDTH)) && ((CURR_HEIGHT==HEIGHT)); do
+  i3-msg "$*"
+  # Provides WINDOW's PID, X start, Y start, WIDTH, HEIGHT
+  eval "$(xdotool getwindowgeometry --shell "$CURR_WINDOW_ID")"
+  i=$((i+1))
+done
 
 NX=$((WIDTH / 2))
 NY=$((HEIGHT / 2))
