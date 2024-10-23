126d642745956ca112ed811f5921813a9c62e3bf
Yasser Kaddoura <yasser.kaddoura19@gmail.com>
2024-10-23 13:19:29 +0300

    fix(qutebrowser): make ad_block finish YouTube ads

--- qutebrowser/.config/qutebrowser/greasemonkey/ad_block.js
+++ qutebrowser/.config/qutebrowser/greasemonkey/ad_block.js
@@ -1,23 +1,22 @@
 // ==UserScript==
 // @name         Auto Skip YouTube Ads
-// @version      1.0.0
-// @description  Speed up and skip YouTube ads automatically
-// @author       jso8910
+// @description  Finish YouTube ads automatically
 // @match        *://*.youtube.com/*
-// @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
-setInterval(() => {
-  const btn = document.querySelector(
-    ".videoAdUiSkipButton,.ytp-ad-skip-button",
-  );
-  if (btn) btn.click();
 
-  const btn1 = document.querySelector(".ytp-ad-skip-button-modern.ytp-button");
-  if (btn1) btn1.click();
-
-  const ad = [...document.querySelectorAll(".ad-showing")][0];
-  if (ad) {
-    document.querySelector("video").playbackRate = 10;
-    document.querySelector("video").muted = true;
+(function() {
+  function callback(mutationList, observer) {
+    if (document.querySelector(".ytp-ad-player-overlay-layout")) {
+      document.querySelector("video").currentTime = document.querySelector("video").duration;
+    }
   }
-}, 50);
+
+  const observerOptions = {
+    childList: true,
+    subtree: true,
+  };
+
+  const targetNode = document.querySelector("ytd-app");
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, observerOptions);
+})();
